c2e000f147e551ca420d5ecafcd2a91b288ab637
Thomas Daniels <daniels.thomas@pm.me>
2025-03-08 19:19:16 +0100

    Add quadratic equation functions

--- src/scalar_func_examples_extension.cpp
+++ src/scalar_func_examples_extension.cpp
@@ -5,6 +5,7 @@
 #include "duckdb/common/exception.hpp"
 #include "duckdb/common/string_util.hpp"
 #include "duckdb/function/scalar_function.hpp"
+#include "duckdb/common/vector_operations/generic_executor.hpp"
 #include "duckdb/main/extension_util.hpp"
 #include <duckdb/parser/parsed_data/create_scalar_function_info.hpp>
 
@@ -53,6 +54,109 @@ inline void DiscriminantScalarFun(DataChunk &args, ExpressionState &state, Vecto
 	                                                                 });
 }
 
+/*struct QuadraticEquationSolution {
+    double x1;
+    double x2;
+    bool exists;
+
+    static void AssignResult(Vector &result, idx_t i, QuadraticEquationSolution value) {
+        auto &entries = StructVector::GetEntries(result);
+        entries[0]->SetValue(i, value.x1);
+        entries[1]->SetValue(i, value.x2);
+        result.validity.Set(i, value.exists);
+    }
+};*/
+
+inline void SolveQuadraticEquationScalarFunc(DataChunk &args, ExpressionState &state, Vector &result) {
+	auto &a_vector = args.data[0];
+	auto &b_vector = args.data[1];
+	auto &c_vector = args.data[2];
+
+	GenericExecutor::ExecuteTernary<PrimitiveType<double_t>, PrimitiveType<double_t>, PrimitiveType<double_t>,
+	                                StructTypeBinary<double_t, double_t>>(
+	    a_vector, b_vector, c_vector, result, args.size(),
+	    [&](PrimitiveType<double_t> a, PrimitiveType<double_t> b, PrimitiveType<double_t> c) {
+		    auto discriminant = b.val * b.val - 4 * a.val * c.val;
+		    StructTypeBinary<double_t, double_t> solution;
+		    /*if (discriminant >= 0) {
+		        solution.exists = true;
+		        solution.x1 = (-b.val + sqrt(discriminant)) / (2 * a.val);
+		        solution.x2 = (-b.val - sqrt(discriminant)) / (2 * a.val);
+		    } else {
+		        solution.exists = false;
+		        solution.x1 = 0;
+		        solution.x2 = 0;
+		    }*/
+		    solution.a_val = (-b.val + sqrt(discriminant)) / (2 * a.val);
+		    solution.b_val = (-b.val - sqrt(discriminant)) / (2 * a.val);
+		    return solution;
+	    });
+}
+
+struct QuadraticEquationSolution {
+	double x1;
+	double x2;
+	bool exists;
+
+	static void AssignResult(Vector &result, idx_t i, QuadraticEquationSolution solution) {
+		if (solution.exists) {
+			child_list_t<Value> values;
+			values.push_back(std::make_pair("x1", solution.x1));
+			values.push_back(std::make_pair("x2", solution.x2));
+			result.SetValue(i, Value::STRUCT(values));
+		} else {
+			result.SetValue(i, Value());
+		}
+	}
+
+	// Alternative implementation, directly working with the vectors:
+	//
+	// static void AssignResult(Vector &result, idx_t i, QuadraticEquationSolution solution) {
+	// 	auto &entries = StructVector::GetEntries(result);
+	// 	entries[0]->SetValue(i, solution.x1);
+	// 	entries[1]->SetValue(i, solution.x2);
+	// 	if (!solution.exists) {
+	// 		FlatVector::SetNull(result, i, true);
+	// 	}
+	// }
+};
+
+inline void SolveQuadraticEquation2ScalarFunc(DataChunk &args, ExpressionState &state, Vector &result) {
+	auto &a_vector = args.data[0];
+	auto &b_vector = args.data[1];
+	auto &c_vector = args.data[2];
+
+	GenericExecutor::ExecuteTernary<PrimitiveType<double_t>, PrimitiveType<double_t>, PrimitiveType<double_t>,
+	                                QuadraticEquationSolution>(
+	    a_vector, b_vector, c_vector, result, args.size(),
+	    [&](PrimitiveType<double_t> a, PrimitiveType<double_t> b, PrimitiveType<double_t> c) {
+		    auto discriminant = b.val * b.val - 4 * a.val * c.val;
+		    QuadraticEquationSolution solution;
+		    if (discriminant >= 0) {
+			    solution.exists = true;
+			    solution.x1 = (-b.val + sqrt(discriminant)) / (2 * a.val);
+			    solution.x2 = (-b.val - sqrt(discriminant)) / (2 * a.val);
+		    } else {
+			    solution.exists = false;
+		    }
+		    return solution;
+	    });
+}
+
+inline void QuadraticEquationFromSolutionScalarFunc(DataChunk &args, ExpressionState &state, Vector &result) {
+	auto &solution_vector = args.data[0];
+
+	GenericExecutor::ExecuteUnary<StructTypeBinary<double_t, double_t>, PrimitiveType<string_t>>(
+	    solution_vector, result, args.size(), [&](StructTypeBinary<double_t, double_t> solution) {
+		    double x1 = solution.a_val;
+		    double x2 = solution.b_val;
+
+		    double b = -x1 - x2;
+		    double c = x1 * x2;
+		    return StringVector::AddString(result, "x^2 + " + to_string(b) + "x + " + to_string(c));
+	    });
+}
+
 static void LoadInternal(DatabaseInstance &instance) {
 	auto quack_scalar_function = ScalarFunction("quack", {LogicalType::VARCHAR}, LogicalType::VARCHAR, QuackScalarFun);
 	ExtensionUtil::RegisterFunction(instance, quack_scalar_function);
@@ -69,6 +173,24 @@ static void LoadInternal(DatabaseInstance &instance) {
 	    ScalarFunction("discriminant", {LogicalType::DOUBLE, LogicalType::DOUBLE, LogicalType::DOUBLE},
 	                   LogicalType::DOUBLE, DiscriminantScalarFun);
 	ExtensionUtil::RegisterFunction(instance, discriminant_scalar_function);
+
+	child_list_t<LogicalType> quadratic_equation_solution_child_types;
+	quadratic_equation_solution_child_types.push_back(std::make_pair("x1", LogicalType::DOUBLE));
+	quadratic_equation_solution_child_types.push_back(std::make_pair("x2", LogicalType::DOUBLE));
+	auto solve_quadratic_equation_scalar_function =
+	    ScalarFunction("solve_quadratic_equation", {LogicalType::DOUBLE, LogicalType::DOUBLE, LogicalType::DOUBLE},
+	                   LogicalType::STRUCT(quadratic_equation_solution_child_types), SolveQuadraticEquationScalarFunc);
+	ExtensionUtil::RegisterFunction(instance, solve_quadratic_equation_scalar_function);
+
+	auto solve_quadratic_equation_scalar_function2 =
+	    ScalarFunction("solve_quadratic_equation2", {LogicalType::DOUBLE, LogicalType::DOUBLE, LogicalType::DOUBLE},
+	                   LogicalType::STRUCT(quadratic_equation_solution_child_types), SolveQuadraticEquation2ScalarFunc);
+	ExtensionUtil::RegisterFunction(instance, solve_quadratic_equation_scalar_function2);
+
+	auto quadratic_equation_from_solution_scalar_function = ScalarFunction(
+	    "quadratic_equation_from_solution", {LogicalType::STRUCT(quadratic_equation_solution_child_types)},
+	    LogicalType::VARCHAR, QuadraticEquationFromSolutionScalarFunc);
+	ExtensionUtil::RegisterFunction(instance, quadratic_equation_from_solution_scalar_function);
 }
 
 void ScalarFuncExamplesExtension::Load(DuckDB &db) {
